5d8f58a8b9353cda8d3ea92212d7dbf4753bfc24
gperman <g.perman@artsofte.ru>
2023-02-28 23:09:11 +0500

    IOT-back-1. add ip

--- config/bundles.php
+++ config/bundles.php
@@ -13,5 +13,4 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
 ];
