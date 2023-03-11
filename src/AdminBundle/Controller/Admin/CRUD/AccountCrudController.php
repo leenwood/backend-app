6dc5586b48f0aec238f9e55fd4c6d6a97798d09e
leenwood <george200135@gmail.com>
2023-03-11 16:34:29 +0500

    IOT-back-2

--- src/AdminBundle/Controller/Admin/CRUD/AccountCrudController.php
+++ src/AdminBundle/Controller/Admin/CRUD/AccountCrudController.php
@@ -3,13 +3,29 @@
 namespace App\AdminBundle\Controller\Admin\CRUD;
 
 use App\AccountBundle\Entity\Account;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AccountCrudController extends AbstractCrudController
 {
+    public function __construct(
+        private UserPasswordHasherInterface $userPasswordHasher
+    )
+    {
+    }
+
+
     public static function getEntityFqcn(): string
     {
         return Account::class;
@@ -19,9 +35,74 @@ class AccountCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+            IdField::new('id')->setDisabled(),
             TextField::new('username'),
             ArrayField::new('roles'),
+            TextField::new('password')
+                ->hideOnDetail()
+                ->hideOnIndex()
+                ->setFormType(RepeatedType::class)
+                ->setFormTypeOptions([
+                    'type' => PasswordType::class,
+                    'first_options' => ['label' => 'Password'],
+                    'second_options' => ['label' => '(Repeat)'],
+                    'mapped' => false,
+                ])
+                ->setRequired($pageName === Crud::PAGE_NEW)
+                ->onlyOnForms(),
         ];
+    }
+
+    /**
+     * @param EntityDto $entityDto
+     * @param KeyValueStore $formOptions
+     * @param AdminContext $context
+     *
+     * @return FormBuilderInterface
+     */
+    public function createNewFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
+    {
+        $formBuilder = parent::createNewFormBuilder($entityDto, $formOptions, $context);
+        return $this->addPasswordEventListener($formBuilder);
+    }
+
+    /**
+     * @param EntityDto $entityDto
+     * @param KeyValueStore $formOptions
+     * @param AdminContext $context
+     *
+     * @return FormBuilderInterface
+     */
+    public function createEditFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
+    {
+        $formBuilder = parent::createEditFormBuilder($entityDto, $formOptions, $context);
+        return $this->addPasswordEventListener($formBuilder);
+    }
+
+    /**
+     * @param FormBuilderInterface $formBuilder
+     *
+     * @return FormBuilderInterface
+     */
+    private function addPasswordEventListener(FormBuilderInterface $formBuilder): FormBuilderInterface
+    {
+        return $formBuilder->addEventListener(FormEvents::POST_SUBMIT, $this->hashPassword());
+    }
+
+    private function hashPassword()
+    {
+        return function($event) {
+            $form = $event->getForm();
+            if (!$form->isValid()) {
+                return;
+            }
+            $password = $form->get('password')->getData();
+            if ($password === null) {
+                return;
+            }
+
+            $hash = $this->userPasswordHasher->hashPassword($this->getUser(), $password);
+            $form->getData()->setPassword($hash);
+        };
     }
 }
